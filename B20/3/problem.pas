a944452db52b6e4f2cd95e4f412c0ea7d82e9d55
Pira66 <pirrrani@mail.ru>
2021-01-03 23:48:01 +0300

    B20/3

---
+++ B20/3/problem.pas
@@ -0,0 +1,28 @@
+program problem;
+
+var
+  i, y, x, r, a, b: integer;
+
+begin
+  for i := 32000 downto -32000 do
+    begin
+      y := i;
+      x := 81;
+      if y > x then
+        begin
+          x := y;
+        end;
+      a := x; b := y;
+      while b > 0 do
+      begin
+        r := a mod b;
+        a := b;
+        b := r;
+      end;
+      if (a = 9) and (x = 81) then
+      break;
+    end;
+  Writeln(i);
+  readln;
+end.
+
