2130c3c374df815dae8db8c943666cd729f6e0f1
Pira66 <pirrrani@mail.ru>
2021-01-04 06:02:18 +0300

    C2/1hasbeenmodified

--- C2/1/problem1.pas
+++ C2/1/problem1.pas
@@ -1,32 +1,34 @@
 program problem1;
 
-const N=20;
+const
+  N = 20;
 
 var
- a: array [1..N] of integer;
- i, j, min: integer;
+  a: array [1..N] of integer;
+  i, j: integer;
 
 begin
+  //случайные числа.
   randomize();
   for i := 1 to N do
-    a[i] := random(1001);//рандомные числа.
+    a[i] := random(1001);
 
+  //вывод случайных чисел.
   for i := 1 to N do
     Write(a[i], ' ');
-  Writeln();             //вывод рандомных чисел.
+  Writeln();
 
-  for i := 1 to N - 1 do
-    for j := 1 to N - i do
-      if (a[j] > a[j + 1]) then
-        begin
-           min := a[j];
-           a[j] := a[j + 1];
-           a[j + 1] := min;
-         end;            //сортировка массива по возрастанию.
+  //минимальное чётное число, которое не делится на 3.
+  for i := 1 to N do
+    if (a[i] mod 2 = 0) and (a[i] mod 3 <> 0) then
+      break;
+  j := a[i];
+  for i := 1 to N do
+    if (a[i] mod 2 = 0) and (a[i] mod 3 <> 0) then
+        if a[i] < j then
+          j := a[i];
 
-for i := 1 to 20 do
-    if (a[i] mod 2 = 0) and (a[i] mod 3 <> 0) then break;
-  Writeln(a[i]);         //минимальное чётное число, которое не делится на 3.
+  //вывод минимального числа, которое не делится на 3.
+  Writeln(j);
 
 end.
-
