bef9e5cbda78cc19d9fff8abc694b00d8955ab5b
Pira66 <pirrrani@mail.ru>
2021-01-04 08:40:10 +0300

    С2/4hasbeenmodified

--- C2/4/problem4.pas
+++ C2/4/problem4.pas
@@ -4,31 +4,36 @@ const
   N = 31;
 
 var
- A: array[1..N] of integer;
- i, x, y: integer;
- s: real;
+  A: array[1..N] of integer;
+  i, x, y: integer;
+  s: real;
 
 begin
+  //рандомные числа.
   randomize();
   for i := 1 to N do
-    a[i] := random (41) - 20;   //рандомные числа.
+    a[i] := random(41) - 20;
 
+  //вывод рандомных чисел.
   for i := 1 to N do
     Write(a[i], ' ');
-  Writeln();                    //вывод рандомных чисел.
+  Writeln();
 
+  //x - сумма температур.
+  //y - кол-во дней с температурой > 0.
   x := 0;
   y := 0;
   for i := 1 to N do
-    begin
-      if a[i] > 0 then
-        x := x + a[i];          //x - сумма температур.
-        y := y + 1;             //y - кол-во дней с температурой > 0.
-    end;
+  begin
+    if a[i] > 0 then
+      x := x + a[i];
+    y := y + 1;
+  end;
 
-  s := x / y;                   //ср. арифм.
+  //ср. арифм.
+  s := x / y;
 
-  Writeln (s:0:2);
+  Writeln(s: 0: 2);
 
 end.
 
