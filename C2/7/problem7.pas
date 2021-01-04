e64b0dce4776794a3fa4ce7cfb78c371c48fcf14
Pira66 <pirrrani@mail.ru>
2021-01-04 09:03:07 +0300

    С2/7hasbeenmodified

--- C2/7/problem7.pas
+++ C2/7/problem7.pas
@@ -4,26 +4,29 @@ const
   N = 30;
 
 var
- a: array [1..N] of integer;
- i, j, x: integer;
+  a: array [1..N] of integer;
+  i, j, x: integer;
 
 begin
+  //случайные числа в диапазоне от -32768 до 32767.
   for i := 1 to N do
-    a[i] := random (65535) - 32768;   //рандомные числа в диапазоне от -32768 до 32767.
+    a[i] := random(65535) - 32768;
 
+  //вывод случайных чисел.
   for i := 1 to N do
     Write(a[i], ' ');
-  Writeln();                          //вывод рандомных чисел.
+  Writeln();
 
   readln(x);
 
+  //если заданное число есть в массиве, то j примет значение 1.
   j := 0;
   for i := 1 to N do
     if a[i] = x then
     begin
       j := j + 1;
       break;
-    end;                              //если заданное число есть в массиве, то j примет значение 1.
+    end;
 
   if j = 1 then
     Writeln(i)
