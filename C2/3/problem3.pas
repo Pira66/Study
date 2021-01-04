44ecdc9eea30cf992e247069e02329ee6764eee4
Pira66 <pirrrani@mail.ru>
2021-01-04 08:36:25 +0300

    С2/3hasbeenmodified

--- C2/3/problem3.pas
+++ C2/3/problem3.pas
@@ -4,31 +4,36 @@ const
   N = 30;
 
 var
- A: array[1..N] of integer;
- i, x, y: integer;
- s: real;
+  A: array[1..N] of integer;
+  i, x, y: integer;
+  s: real;
 
 begin
+  //рандомные числа.
   randomize();
   for i := 1 to N do
-    a[i] := random(41) + 160;   //рандомные числа.
+    a[i] := random(41) + 160;
 
+  //вывод рандомных чисел.
   for i := 1 to N do
     Write(a[i], ' ');
-  Writeln();                    //вывод рандомных чисел.
+  Writeln();
 
+  //x - сумма всех игроков(рост).
+  //y - кол-во игроков, попавших в команду.
   x := 0;
   y := 0;
   for i := 1 to N do
     if a[i] >= 180 then
     begin
-      x := x + a[i];           //x - сумма всех игроков(рост)
-      y := y + 1;              //y - кол-во игроков, попавших в команду
+      x := x + a[i];
+      y := y + 1;
     end;
 
-  s := x / y;                  //среднее арифм.
+  //среднее арифм.
+  s := x / y;
 
-  Writeln(s:0:2);
+  Writeln(s: 0: 2);
 
 end.
 
