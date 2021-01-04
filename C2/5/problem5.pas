17820870fc276c90449af1f146ed3c2d04c8fa3d
Pira66 <pirrrani@mail.ru>
2021-01-04 08:53:53 +0300

    С2/5hasbeenmodified

--- C2/5/problem5.pas
+++ C2/5/problem5.pas
@@ -4,31 +4,30 @@ const
   N = 30;
 
 var
- a: array [1..N] of integer;
- i, j, min: integer;
+  a: array [1..N] of integer;
+  i, j: integer;
 
 begin
+  //случайные числа.
   randomize();
   for i := 1 to N do
-    a[i] := random(101);   //рандомные числа.
+    a[i] := random(101);
 
+  //вывод случайных чисел.
   for i := 1 to N do
     Write(a[i], ' ');
-  Writeln();               //вывод рандомных чиел.
+  Writeln();
 
-  for i := 1 to N - 1 do
-    for j := 1 to N - i do
-        if a[j] > a[j + 1] then
-        begin
-          min := a[j];
-          a[j] := a[j + 1];
-          a[j + 1] := min;
-        end;               //сортировка массива по возрастанию.
-
+  //минимальный проходной балл.
   for i := 1 to N do
-    if a[i] > 20 then break;
+    if a[i] > 20 then
+      break;
+  j := a[i];
+  for i := 1 to N do
+    if a[i] > 20 then
+      if a[i] < j then
+        j := a[i];
 
-  Writeln(a[i]);           //минимальный балл.
+  Writeln(j);
 
 end.
-
