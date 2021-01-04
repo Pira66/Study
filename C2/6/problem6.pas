a1880a579d78b578dcb811358f48a1b767e4cc45
Pira66 <pirrrani@mail.ru>
2021-01-04 08:59:56 +0300

    С2/6hasbeenmodified

--- C2/6/problem6.pas
+++ C2/6/problem6.pas
@@ -2,33 +2,36 @@ program problem6;
 
 
 const
-  N=30;
+  N = 30;
 
 var
- a: array [1..N] of integer;
- i, j: integer;
- s: real;
+  a: array [1..N] of integer;
+  i, j: integer;
+  s: real;
 
 begin
+  //случайные числа.
   randomize();
   for i := 1 to N do
-    a[i] := random(101);   //рандомные числа.
+    a[i] := random(101);
 
+  //вывод случайных чисел.
   for i := 1 to N do
     Write(a[i], ' ');
-  Writeln();               //вывод рандомных чисел.
+  Writeln();
 
+  //средний балл.
   for i := 1 to N do
     j += a[i];
+  s := j / N;
 
-  s := j / N;              //средний балл.
-
+  //кол-во учеников, у которых балл выше среднего.
   j := 0;
   for i := 1 to N do
     if a[i] > s then
       j := j + 1;
 
-  Writeln(j);              //кол-во учеников, у которых балл выше среднего.
+  Writeln(j);
 
 end.
 
