d486a14ff0e66c2a4c62042c749cc8643c601b4f
Pira66 <pirrrani@mail.ru>
2021-01-07 01:29:37 +0300

    B16/20

---
+++ B16/20/problem20.pas
@@ -0,0 +1,23 @@
+program problem20;
+
+function G(n: integer): integer; forward;
+
+function F(n: integer): integer;
+begin
+  if n = 1 then
+    F := 1
+  else
+    F := 3 * F(n - 1) + G(n - 1) - n + 5;
+end;
+
+function G(n: integer): integer;
+begin
+  if n = 1 then
+    G := 1
+  else
+    G := F(n - 1) + 3 * G(n - 1) -  3 * n
+end;
+
+begin
+  Writeln(F(14) + G(14));
+end.
