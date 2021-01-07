449b6482c2be6fbe8a885f42285348ab030f9841
Pira66 <pirrrani@mail.ru>
2021-01-08 01:04:39 +0300

    С2/11

---
+++ C2/11/problem11.pas
@@ -0,0 +1,30 @@
+program problem11;
+
+const
+  N = 30;
+
+var
+  a: array [1..N] of integer;
+  i, min: integer;
+
+begin
+  //случайные числа.
+  randomize();
+  for i := 1 to N do
+    a[i] := random(10001);
+
+  //вывод случайных чисел.
+  for i := 1 to N do
+    Write(a[i], ' ');
+  Writeln();
+
+  //минимальное трехзначное число.
+  min := 10001;
+  for i := 1 to N do
+    if (a[i] < min) and (a[i] div 1000 = 0) then
+      min := a[i];
+
+  Writeln(min);
+
+end.
+
