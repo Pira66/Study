73e8e7647b7d8632e0781ce243c9795597250502
Pira66 <pirrrani@mail.ru>
2021-01-08 02:15:58 +0300

    С2/15

---
+++ C2/15/problem15.pas
@@ -0,0 +1,36 @@
+program problem15;
+
+const
+  N = 30;
+
+var
+  a: array [1..N] of integer;
+  i, j, s: integer;
+
+begin
+  //случайные числа.
+  randomize();
+  for i := 1 to N do
+    a[i] := random(1001);
+
+  //вывод сулчайных чисел.
+  for i := 1 to N do
+    Write(a[i], ' ');
+  Writeln();
+
+  //среднее арифметическое нечётных трехзначных чисел.
+  s := 0;
+  j := 0;
+  for i := 1 to N do
+    if (a[i] div 1000 = 0) and (a[i] mod 1000 <> 0) and (a[i] mod 2 <> 0) then
+    begin
+      s += a[i];
+      j += 1;
+    end;
+
+  if j <> 0 then
+    Writeln((s / j):0 :2) else
+    Writeln('Takih znacheniy net');
+
+end.
+
