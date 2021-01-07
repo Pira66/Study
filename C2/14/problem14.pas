0033d4007f83e72f56a05ac7fa8ce0a2a62fbf26
Pira66 <pirrrani@mail.ru>
2021-01-08 01:50:27 +0300

    C2/14

---
+++ C2/14/problem14.pas
@@ -0,0 +1,28 @@
+program problem14;
+
+const N = 30;
+
+var
+  a: array [1..N] of integer;
+  i, s: integer;
+
+begin
+  //случайные числа.
+  randomize();
+  for i := 1 to N do
+    a[i] := random(1001);
+
+  //вывод случайных чисел.
+  for i := 1 to N do
+    Write(a[i], ' ');
+  Writeln();
+
+  //сумма элементов массива, кратных тринадцати.
+  for i := 1 to N do
+    if a[i] mod 13 = 0 then
+      s += a[i];
+
+  Writeln(s);
+
+end.
+
