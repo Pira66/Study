b7361cc1e3bab8b4894c1424762f5a47b001c970
Pira66 <pirrrani@mail.ru>
2021-01-10 00:10:09 +0300

    modified

--- B16/28/program28.pas
+++ B16/28/program28.pas
@@ -3,28 +3,25 @@ program program28;
 var
   n, a: integer;
 
-procedure F(n: integer; var a: integer);
-begin
-  a := a + 2 * n + 1;
-  if n > 1 then
+  procedure F(n: integer);
   begin
-    a := a + 3 * n - 8;
-    n -= 1;
-    F(n, a);
-    n-=4;
-    F(n, a);
+    a := a + 2 * n + 1;
+    if n > 1 then
+    begin
+      a := a + 3 * n - 8;
+      F(n - 1);
+      F(n - 4);
+    end;
   end;
-end;
 
 begin
   n := 1;
   repeat
-    Writeln(a);
     a := 0;
     n += 1;
-    F(n, a);
+    F(n);
   until a > 5000000;
 
-  Writeln(n, ' ',a);//45
+  Writeln(n, ' ', a);
 end.
 
