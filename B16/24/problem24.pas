79d97ebc85339f36726405092c13d9ac7a3a91be
Pira66 <pirrrani@mail.ru>
2021-01-10 00:04:59 +0300

    modified

--- B16/24/problem24.pas
+++ B16/24/problem24.pas
@@ -3,20 +3,21 @@ program problem24;
 var
   k: integer;
 
-function F(n: integer; var k: integer): integer;
+function F(n: integer): integer;
 begin
   k := k + 1;
   if n >= 1 then
   begin
     k := k + 1;
-    F(n-1, k);
-    F(n div 3, k);
+    F(n-1);
+    F(n div 3);
     k := k + 1;
   end;
 end;
 
 begin
   k := 0;
-  Writeln(F(280, k));
+  F(280);
+  Writeln(k);
 end.
 
