c9973b10013b7d208877f51da8793cd9eaaddb13
Pira66 <pirrrani@mail.ru>
2021-01-11 19:08:41 +0300

    B16/48

---
+++ B16/48/problem48.pas
@@ -0,0 +1,25 @@
+program problem48;
+
+var
+  k: integer;
+
+function F(n: integer): integer;
+begin
+  if n <= 3 then
+    F := n;
+  if (n mod 2 = 0) and (n > 3) then
+    F := n + F(n - 1);
+  if (n mod 2 <> 0) and (n > 3) then
+    F := n * n + F(n - 2);
+end;
+
+begin
+  k := 1;
+  While F(k) < 2 do
+  begin
+    k += 1;
+  end;
+
+  Writeln(k);
+end.
+
