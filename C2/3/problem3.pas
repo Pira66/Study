9eada0d9f5bbd74bd305661d486f6a57fbeae128
Pira66 <pirrrani@mail.ru>
2021-01-11 20:25:43 +0300

    modified

--- C2/3/problem3.pas
+++ C2/3/problem3.pas
@@ -5,8 +5,7 @@ const
 
 var
   A: array[1..N] of integer;
-  i, x, y: integer;
-  s: real;
+  i, x: integer;
 
 begin
   //случайные числа.
@@ -19,21 +18,13 @@ begin
     Write(a[i], ' ');
   Writeln();
 
-  //x - сумма всех игроков(рост).
-  //y - кол-во игроков, попавших в команду.
-  x := 0;
-  y := 0;
+  //самый низкий игрок баскетбольной команды.
+  x := 201;
   for i := 1 to N do
-    if a[i] >= 180 then
-    begin
-      x := x + a[i];
-      y := y + 1;
-    end;
+    if (a[i] >= 180) and (a[i] < x) then
+      x := a[i];
 
-  //среднее арифм.
-  s := x / y;
-
-  Writeln(s: 0: 2);
+  Writeln(x);
 
 end.
 
