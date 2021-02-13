c26870e884450d00fc62fc352751a1881a4de4a6
Pira66 <pirrrani@mail.ru>
2021-02-13 15:31:25 +0300

    C4/simple2

---
+++ C4/2/simple2.bat
@@ -0,0 +1,9 @@
+@echo off
+G:\lazarus\fpc\3.2.0\bin\x86_64-win64\fpc.exe simple2.exe
+simple2.exe < tests\01 > tests\01.out
+simple2.exe < tests\02 > tests\02.out
+simple2.exe < tests\03 > tests\03.out
+fc /A tests\01.out tests\01.a
+fc /A tests\02.out tests\02.a
+fc /A tests\03.out tests\03.a
+del tests\*.out 
