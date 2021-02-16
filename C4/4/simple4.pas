f331f8b9d134ce942f580946568b855ed51975ff
Pira66 <pirrrani@mail.ru>
2021-02-17 00:27:31 +0300

    modified

--- C4/4/simple4.pas
+++ C4/4/simple4.pas
@@ -5,8 +5,8 @@ const
 
 var
   a: array[1..Lim] of real;
-  i, j, n, f, q: integer;
-  min: real;
+  i, j, n, f, fm, q: integer;
+  min, mmin: real;
 
 begin
   //ввод кол-ва элементов и их значений
@@ -16,6 +16,7 @@ begin
 
   //произведение чисел будет максимальным, если каждое будет > 1(записываем кол-во таких чисел и минимальное среди них)
   f := 0;
+  fm := 0;
   q := 0;
   for i := 1 to n do
     if a[i] > 1 then
@@ -29,9 +30,20 @@ begin
       else
       if a[i] < min then
         min := a[i];
+      if (a[i] < 1) and (a[i] > 0) then
+        if fm = 0 then
+        begin
+          mmin := a[i];
+          fm += 1;
+        end
+        else
+          if a[i] > mmin then
+            mmin := a[i];
     end;
 
-  Writeln(q, ' ', min: 0: 1);
-
+  if q <> 0 then
+    Writeln(q, ' ', min: 0: 1)
+  else
+    Writeln(1, mmin);
 end.
 
