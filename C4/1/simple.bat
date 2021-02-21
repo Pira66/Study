eaaa9ac4800ba10c8757838182e7c0a30341826a
Pira66 <pirrrani@mail.ru>
2021-02-22 02:23:57 +0300

    bat

--- C4/1/simple.bat
+++ C4/1/simple.bat
@@ -1,9 +1,8 @@
 @echo off
-G:\lazarus\fpc\3.2.0\bin\x86_64-win64\fpc.exe simple.exe
-simple.exe < tests\01 > tests\01.out
-simple.exe < tests\02 > tests\02.out
-simple.exe < tests\03 > tests\03.out
-fc /A tests\01.out tests\01.a
-fc /A tests\02.out tests\02.a
-fc /A tests\03.out tests\03.a
-del tests\*.out
+fpc simple.lpr > NUL
+for %%a in (tests\*.a) do (
+  simple.exe < tests\%%~na > tests\%%~na.out
+  fc /A tests\%%~na.out tests\%%~na.a
+  del tests\%%~na.out
+)
+del simple.exe
