9fa700ed851c6ec30a55c879450145ff3a897602
Pira66 <pirrrani@mail.ru>
2021-02-25 17:53:51 +0300

    full

--- C4/2/full_search2.bat
+++ C4/2/full_search2.bat
@@ -1,8 +1,8 @@
 @echo off
-fpc simple2.pas > NUL
+fpc full_search2.pas > NUL
 for %%a in (tests\*.a) do (
-  simple2.exe < tests\%%~na > tests\%%~na.out
+  full_search2.exe < tests\%%~na > tests\%%~na.out
   fc /A tests\%%~na.out tests\%%~na.a
   del tests\%%~na.out
 )
-del simple2.exe
+del full_search2.exe
