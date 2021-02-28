d27ccd9ff73694713c4af2b0428176ca3acc5ad7
Pira66 <pirrrani@mail.ru>
2021-02-28 17:26:42 +0300

    modified

--- C4/3/full_search.bat
+++ C4/3/full_search.bat
@@ -1,8 +1,8 @@
 @echo off
-fpc full_search3.pas > NUL
+fpc full_search.pas > NUL
 for %%a in (tests\*.a) do (
-  full_search3.exe < tests\%%~na > tests\%%~na.out
+  full_search.exe < tests\%%~na > tests\%%~na.out
   fc /A tests\%%~na.out tests\%%~na.a
   del tests\%%~na.out
 )
-del full_search3.exe
+del full_search.exe
