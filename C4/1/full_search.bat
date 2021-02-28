7aa1cfe8166e2992add567887f7b9646e36f1336
Pira66 <pirrrani@mail.ru>
2021-02-28 17:23:06 +0300

    modified

--- C4/1/full_search.bat
+++ C4/1/full_search.bat
@@ -1,8 +1,8 @@
 @echo off
-fpc full_search1.lpr > NUL
+fpc full_search.lpr > NUL
 for %%a in (tests\*.a) do (
-  full_search1.exe < tests\%%~na > tests\%%~na.out
+  full_search.exe < tests\%%~na > tests\%%~na.out
   fc /A tests\%%~na.out tests\%%~na.a
   del tests\%%~na.out
 )
-del full_search1.exe
+del full_search.exe
