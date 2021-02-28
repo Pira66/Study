7888c963b66ed8e6d51c3851f75f44165dfdb8be
Pira66 <pirrrani@mail.ru>
2021-02-28 20:30:43 +0300

    modified

--- C4/2/tests_gen.pas
+++ C4/2/tests_gen.pas
@@ -1,29 +1,18 @@
-program tests_gen2;//для задания 2
+program tests_gen;
 
 Uses
   SysUtils;
 
 var
   a: TextFile;
-  i, j: integer;
-  q: integer;   //кол-во тестов
-  n: integer;   //кол-во строк в тестах
-  s: string;
-
-procedure write_test_data_to_file(s: string);
-begin
-  Assign(a, s);
-  Rewrite(a);
-  Writeln(a, n);
-  for j := 1 to n do
-    Writeln(a, random(3001));
-  Close(a);
-end;
+  i: integer;
+  n, lim: integer;
 
 begin
+  n := StrToInt(Paramstr(1));
+  lim := StrToInt(Paramstr(2));
+  Writeln(n);
   randomize();
-  q := StrToInt(Paramstr(1));
-  n := StrToInt(Paramstr(2));
-  for i := 1 to q do
-    write_test_data_to_file(IntToStr(i) + '.txt');
+  for i := 1 to n do
+    Writeln(random(lim));
 end.
