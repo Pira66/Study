9e6f20aa9cab8961083ea1b3ec58cebb88fb4280
Pira66 <pirrrani@mail.ru>
2021-02-28 17:12:22 +0300

    renamed

--- C4/2/full_search2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-fpc full_search2.pas > NUL
-for %%a in (tests\*.a) do (
-  full_search2.exe < tests\%%~na > tests\%%~na.out
-  fc /A tests\%%~na.out tests\%%~na.a
-  del tests\%%~na.out
-)
-del full_search2.exe
