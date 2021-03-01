5b5c17887639c01a662f462f512763387eb7dd07
Pira66 <pirrrani@mail.ru>
2021-03-01 17:53:21 +0300

    modified

--- C4/1/optimal.pas
+++ C4/1/optimal.pas
@@ -7,110 +7,41 @@ const
   LIM = 100;
 
 var
-  x: array[1..LIM] of integer;
-  y: array[1..LIM] of integer;
-  i, n, fxl, fyl, fxr, fyr ,xmaxXr, xmaxYr, xminXr, xminYr , ymaxXr, ymaxyr, xmaxXl, xmaxYl, xminXl, xminYl, ymaxXl, ymaxYl: integer;
-  al, bl, cl, ar, br, cr, SL, SR, PL, PR: real;
+  i, n, x, y: integer;
+  a, b, c, d, e, f: integer;
+  fa, fb ,fc, fd, fe, ff: integer;
+
+procedure p(var a, b, c, fa, fb, fc, x, y: integer);
+begin
+  if (y = 0) and ((x < a) or (fa = 0)) then
+  begin
+    fa := 1;
+    a := x;
+  end;
+  if (y = 0) and ((x > b) or (fb = 0)) then
+  begin
+    fb := 1;
+    b := x;
+  end;
+  if (abs(y) > c) or (fc = 0) then
+  begin
+    fc := 1;
+    c := abs(y);
+  end;
+end;
+
 begin
   readln(n);
   for i := 1 to n do
-    readln(x[i], y[i]);
-
-  fxl := 0;
-  fyl := 0;
-  fxr := 0;
-  fyr := 0;
-  for i := 1 to n do
   begin
-    if x[i] < 0 then
-      if y[i] = 0 then
-        if fxl = 0 then
-        begin
-          fxl += 1;
-          xminXl := x[i];
-          xminYl := y[i];
-          xmaxXl := x[i];
-          xmaxYl := y[i];
-        end
-        else
-        begin
-          if x[i] < xminXl then
-          begin
-            xminXl := x[i];
-            xminYl := y[i];
-          end;
-          if x[i] > xmaxXl then
-          begin
-            xmaxXl := x[i];
-            xmaxYl := y[i];
-          end;
-        end
-      else
-        if fyl = 0 then
-        begin
-          fyl += 1;
-          ymaxXl := x[i];
-          ymaxYl := y[i];
-        end
-        else
-          if abs(y[i]) > abs(ymaxYl) then
-          begin
-            ymaxXl := x[i];
-            ymaxYl := y[i];
-          end;
-    if x[i] > 0 then
-      if y[i] = 0 then
-        if fxr = 0 then
-        begin
-          fxr += 1;
-          xminXr := x[i];
-          xminYr := y[i];
-          xmaxXr := x[i];
-          xmaxYr := y[i];
-        end
-        else
-        begin
-          if x[i] < xminXr then
-          begin
-            xminXr := x[i];
-            xminYr := y[i];
-          end;
-          if x[i] > xmaxXr then
-          begin
-            xmaxXr := x[i];
-            xmaxYr := y[i];
-          end;
-        end
-      else
-        if fyr = 0 then
-        begin
-          fyr += 1;
-          ymaxXr := x[i];
-          ymaxYr := y[i];
-        end
-        else
-          if abs(y[i]) > abs(ymaxYr) then
-          begin
-            ymaxXr := x[i];
-            ymaxYr := y[i];
-          end;
+    readln(x, y);
+    if x < 0 then
+      p(a, b, c, fa, fb, fc, x, y);
+    if x > 0 then
+      p(d, e, f, fd, fe, ff, x, y);
   end;
 
-  al := sqrt(sqr(ymaxXl - xminXl) + sqr(ymaxYl - xminYl));
-  bl := sqrt(sqr(xmaxXl - ymaxXl) + sqr(xmaxYl - ymaxYl));
-  cl := sqrt(sqr(xminXl - xmaxXl) + sqr(xminYl - xmaxYl));
-
-  ar := sqrt(sqr(ymaxXr - xminXr) + sqr(ymaxYr - xminYr));
-  br := sqrt(sqr(xmaxXr - ymaxXr) + sqr(xmaxYr - ymaxYr));
-  cr := sqrt(sqr(xminXr - xmaxXr) + sqr(xminYr - xmaxYr));
-
-  PL := (al + bl + cl) / 2;
-  PR := (ar + br + cr) / 2;
-
-  SR := sqrt(PR * (PR - ar) * (PR - br) * (PR - cr));
-  SL := sqrt(PL * (PL - al) * (PL - bl) * (PL - cl));
-
-  Writeln(max(SR, SL): 0: 1);
+  Writeln(max((b - a)*c, (e - d)*f)/2: 0: 1);
 
 end.
 
