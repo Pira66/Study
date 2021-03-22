3fb1b7442527dc499d8856da540eb0f4e12a5390
Pira66 <pirrrani@mail.ru>
2021-03-23 00:32:26 +0300

    c4/2

--- C4/2/optimal2.pas
+++ C4/2/optimal2.pas
@@ -3,65 +3,46 @@ program optimal2;
 uses
   Math;
 
+const
+  INF = 60000;
+
 var
   i, n, x: integer;
-  a, b, c, d, e, f, ci, cii: longint;
-  fo, ft: integer;
+  a, b, c, d, e, f: longint;
+
+procedure update_min(var a, b: longint; x: longint);
+begin
+  if x <= a then
+  begin
+    b := a;
+    a := x;
+  end
+  else
+    if x <= b then
+      b := x;
+end;
 
 begin
   readln(n);
+  a := INF;
+  b := INF;
+  c := INF;
+  d := INF;
+  e := INF;
+  f := INF;
 
-  fo := 0; ft := 0;
-  readln(x);
-  a := x;
-  c := 30001; d := 30001;
-  e := 30001; f := 30001;
   for i := 1 to n do
   begin
     readln(x);
-    if x < a then
-    begin
-      a := x;
-      cii := i;
-    end;
-    if (x < b) and (x >= a) and (cii <> i) or (i = 2) then
-      b := x;
-
-    if (x < c) and (x mod 2 = 0) then
-    begin
-      d := c;
-      c := x;
-      ci := i;
-      if d <> 30001 then
-        fo := 1;
-    end;
-    if (x < d) and (x >= c) and (x mod 2 = 0) and (i <> ci) then
-    begin
-      d := x;
-      fo := 1;
-    end;
-
-    if (x < e) and (x mod 2 <> 0) then
-    begin
-      f := e;
-      e := x;
-      if f <> 30001 then
-        fo := 1;
-    end;
-    if (x < f) and (x > e) and (x mod 2 <> 0) then
-    begin
-      f := x;            if d <> 30001 then
-        fo := 1;
-      ft := 1;
-    end;
+    update_min(a, b, x);
+    if x mod 2 = 0 then
+      update_min(c, d, x)
+    else
+      update_min(e, f, x)
   end;
 
-  if (fo = 1) and (ft = 1) then
-    Writeln(min(c + d, e + f));
-  if (fo = 1) and (ft = 0) then
-    Writeln(c + d);
-  if (ft = 1) and (fo = 0) then
-    Writeln(e + f);
-  if (fo = 0) and (ft = 0) then
+  if (d < INF) or (f < INF) then
+    Writeln(min(c + d, e + f))
+  else
     Writeln(a + b);
 end.
