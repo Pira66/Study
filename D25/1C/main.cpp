c74db37eb33a77e79829e5dad9506ec5ffeda8b8
Pira66 <pirrrani@mail.ru>
2021-03-30 00:50:28 +0300

    с1 d25

---
+++ D25/1C/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void p(int i, int& a, int& b, int& q){
+  q = 0;
+  for (int j = 2; j < i; j++)
+    if (i % j == 0){
+      q ++;
+      a = q == 1 ? j : a;
+      b = q == 2 ? j : b;
+    }
+}
+
+int main(){
+  freopen("tests\\01", "r", stdin);
+
+  int x, y;
+  cin >> x >> y;
+
+  int a, b, q:
+  for (int i = x; i <= y; i ++){
+    p(i, a, b, q);
+    if (q == 2)
+      cout << a << " " << b << endl;
+  }
+
+  return 0;
+
+}
