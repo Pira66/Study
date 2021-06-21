2c182c98ee2dfaa222b604bc4730e181812ee98a
Pira66 <pirrrani@mail.ru>
2021-06-21 22:08:53 +0300

    task229

---
+++ other/task229/task229.pas
@@ -0,0 +1,26 @@
+﻿program task229;
+
+uses
+  School;
+
+var
+  s: string;
+  N, q, a: integer;
+
+begin
+  for N := 2 to 32000 do
+  begin
+    s := bin(N);
+        
+    s += s[length(s) - 1];
+    
+    s += s[2];
+    
+    a := dec(s, 2);
+    
+    if (a >= 100) and (a <= 150) then
+      q += 1;
+  end;
+
+  Writeln(q);
+end.
